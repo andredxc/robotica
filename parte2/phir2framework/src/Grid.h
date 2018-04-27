60feabd191e01aa1d60e685bd2808c4d63120b7f
Andé <andre.dxc@hotmail.com>
2018-04-27 17:59:17 -0300

    Adicionando arquivos da parte 2

---
+++ parte2/phir2framework/src/Grid.h
@@ -0,0 +1,44 @@
+#ifndef __GRID_H__
+#define __GRID_H__
+
+class Cell
+{
+    public:
+        int x,y;
+
+        double logodds, occupancy;
+        double logoddsSonar,occupancySonar;
+        int himm;
+
+        void updateOccupancyFromLogOdds();
+};
+
+class Grid
+{
+    public:
+        Grid();
+        Cell* getCell(int x, int y);
+
+        int getMapScale();
+        int getMapWidth();
+        int getMapHeight();
+
+        void draw(int xi, int yi, int xf, int yf);
+
+        int numViewModes;
+        int viewMode;
+        bool showValues;
+
+    private:
+        int mapScale_; // Number of cells per meter
+        int mapWidth_, mapHeight_; // in cells
+        int numCellsInRow_, halfNumCellsInRow_;
+
+        Cell* cells_;
+
+        void drawCell(unsigned int i);
+        void drawVector(unsigned int i);
+        void drawText(unsigned int n);
+};
+
+#endif // __GRID_H__
